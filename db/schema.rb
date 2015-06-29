15199ca70d623fd0837cf4ac0f490e5b29102fd6
erica bista <YOUR EMAIL ADDRESSericabista77@gmail.com>
2015-06-29 14:02:29 +0545

    name added to devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150624082956) do
+ActiveRecord::Schema.define(version: 20150629081050) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -126,6 +126,7 @@ ActiveRecord::Schema.define(version: 20150624082956) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
